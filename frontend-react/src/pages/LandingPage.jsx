133930ec80e4589e3588600c6015a4f20738549d
mrunal-mm <mrunalmm2@gmail.com>
2024-06-16 18:53:34 +0530

    fe: Created Navbar1 component added logo and created LandingPage

---
+++ frontend-react/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Navbar1 from "../components/Navbar1"
+
+function LandingPage(){
+    return(
+        <div>
+            <Navbar1></Navbar1>
+        </div>
+    )
+}
+
+export default LandingPage;
