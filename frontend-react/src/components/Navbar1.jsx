b4bd67bf16ae2bb9821ff7ba70f251a0d478b91f
mrunal-mm <mrunalmm2@gmail.com>
2024-06-16 21:11:36 +0530

    Created re login-register containers for voters, candidates and parties

--- frontend-react/src/components/Navbar1.jsx
+++ frontend-react/src/components/Navbar1.jsx
@@ -2,19 +2,19 @@ import logo from '../assets/images/logo.png'
 
 function Navbar1() {
     return (
-     <nav className='padding-10 shadow'>
-        <div className='nav-left-container'>
-            <img src={logo} alt="" height="60px" width="60px"/>
-            <span className='font-mont nav-heading'>Election Commission</span>
-        </div>
+         <nav className='padding-10 shadow'>
+            <div className='nav-left-container'>
+                <img src={logo} alt="" height="60px" width="60px"/>
+                <span className='font-mont nav-heading'>Election Commission</span>
+            </div>
 
-        <div className='nav-right-container'>
-            <a>About Us</a>
-            <a>Contact Us</a>
-            <button className='btn btn-outline-blue'>Login</button>
-            <button className='btn btn-blue'>Register</button>
-        </div>
-     </nav>   
+            <div className='nav-right-container'>
+                <a>About Us</a>
+                <a>Contact Us</a>
+                <button className='btn btn-outline-blue'>Login</button>
+                <button className='btn btn-blue'>Register</button>
+            </div>
+         </nav>  
     )
 }
 
--- frontend-react/src/pages/LandingPage.jsx
+++ frontend-react/src/pages/LandingPage.jsx
@@ -1,11 +1,11 @@
 import main_image from '../assets/images/landing-page-main.png'
-
 import Navbar1 from "../components/Navbar1"
 
-function LandingPage(){
-    return(
+function LandingPage() {
+    return (
         <div>
             <Navbar1></Navbar1>
+
             <div className="landing-page-introduction padding-10">
                 <div className='introduction-container'>
                     <p className='introduction-heading font-mont'>Election</p>
@@ -13,7 +13,28 @@ function LandingPage(){
                     <p className='introduction-subheading mt-4'>Safeguarding Democracy,</p>
                     <p className='introduction-subheading'>Ensuring Fair Elections</p>
                 </div>
-                <img src={main_image} alt="" className='introduction-image'/>
+                <img src={main_image} alt="" className='introduction-image' />
+            </div>
+
+            <div className="user-section-container mt-5 mb-5 padding-10">
+                <div className="user-section">
+                    <h1>Parties</h1>
+                    <button type='button' className='btn btn-outline-blue'>Party Registration</button>
+                    <button type='button' className='btn btn-outline-blue'>Manage Applicants</button>
+                    <button type='button' className='btn btn-blue'>Login</button>
+                </div>
+                <div className="user-section">
+                    <h1>Voters</h1>
+                    <button type='button' className='btn btn-outline-blue'>Register To Vote</button>
+                    <button type='button' className='btn btn-outline-blue'>Download Voter ID</button>
+                    <button type='button' className='btn btn-blue'>Login</button>
+                </div>
+                <div className="user-section">
+                    <h1>Candidates</h1>
+                    <button type='button' className='btn btn-outline-blue'>Register as Candidate</button>
+                    <button type='button' className='btn btn-outline-blue'>Aplication Status</button>
+                    <button type='button' className='btn btn-blue'>Login</button>
+                </div>
             </div>
         </div>
     )
