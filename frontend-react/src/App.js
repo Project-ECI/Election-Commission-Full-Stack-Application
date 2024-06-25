3e47b4d9b80b8b1cf2da9e4742534ee60c25e165
sagar <sagardudhe711@gmail.com>
2024-06-25 22:53:57 +0530

    CastVote

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import VoterLoginPage from "./pages/Voter/VoterLogin";
 import VoterRegPage from "./pages/Voter/VoterRegistration";
-import CandidateRegPage from "./pages/Candidate/CandidateRegistration"
+import CandidateRegPage from "./pages/Candidate/CandidateRegistration";
 import CandidateLoginPage from "./pages/Candidate/CandidateLogin";
 import PartyLoginPage from "./pages/Party/PartyLogin";
 import PartyRegPage from "./pages/Party/PartyRegistration";
+import CastVote from "./pages/Voter/CastVote";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -13,13 +14,13 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/voter-reg" element={<VoterRegPage/>} />
-        <Route path="/voter-login" element={<VoterLoginPage/>} />
-        <Route path="/candidate-reg" element={<CandidateRegPage/>} />
-        <Route path="/candidate-login" element={<CandidateLoginPage/>} />
-        <Route path="/party-reg" element={<PartyRegPage/>} />
-        <Route path="/party-login" element={<PartyLoginPage/>} />
+        <Route path="/" element={<CastVote />} />
+        <Route path="/voter-reg" element={<VoterRegPage />} />
+        <Route path="/voter-login" element={<VoterLoginPage />} />
+        <Route path="/candidate-reg" element={<CandidateRegPage />} />
+        <Route path="/candidate-login" element={<CandidateLoginPage />} />
+        <Route path="/party-reg" element={<PartyRegPage />} />
+        <Route path="/party-login" element={<PartyLoginPage />} />
       </Routes>
     </div>
   );
