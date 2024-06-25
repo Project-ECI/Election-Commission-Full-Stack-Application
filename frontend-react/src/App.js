9d95d72cebf05d63535fba635ed1273093d3cb7e
sagar <sagardudhe711@gmail.com>
2024-06-26 00:19:49 +0530

    create know your candidate page and update voting page

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -4,7 +4,7 @@ import VoterLoginPage from "./pages/Voter/VoterLogin";
 import VoterRegPage from "./pages/Voter/VoterRegistration";
 import VoterHomepage from "./pages/Voter/VoterHomepage";
 
-import CandidateRegPage from "./pages/Candidate/CandidateRegistration"
+import CandidateRegPage from "./pages/Candidate/CandidateRegistration";
 import CandidateLoginPage from "./pages/Candidate/CandidateLogin";
 
 import PartyLoginPage from "./pages/Party/PartyLogin";
@@ -12,23 +12,25 @@ import PartyRegPage from "./pages/Party/PartyRegistration";
 import VoterGuide from "./pages/FooterLinks/VoterArticle";
 import PartyArticle from "./pages/FooterLinks/PartyArticle";
 import CastVote from "./pages/Voter/CastVote";
-
+import KnowYourCandidate from "./pages/FooterLinks/KnowYourCandidate";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/voter-reg" element={<VoterRegPage/>} />
-        <Route path="/voter-login" element={<VoterLoginPage/>} />
-        <Route path="/candidate-reg" element={<CandidateRegPage/>} />
-        <Route path="/candidate-login" element={<CandidateLoginPage/>} />
-        <Route path="/party-reg" element={<PartyRegPage/>} />
-        <Route path="/party-login" element={<PartyLoginPage/>} />
-        <Route path="/become-good-voter" element={<VoterGuide/>} />
-        <Route path="/become-good-party" element={<PartyArticle/>} />
-        <Route path="/voter-homepage" element={<VoterHomepage/>} />
-        <Route path="/" element={<CastVote />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/voter-reg" element={<VoterRegPage />} />
+        <Route path="/voter-login" element={<VoterLoginPage />} />
+        <Route path="/candidate-reg" element={<CandidateRegPage />} />
+        <Route path="/candidate-login" element={<CandidateLoginPage />} />
+        <Route path="/party-reg" element={<PartyRegPage />} />
+        <Route path="/party-login" element={<PartyLoginPage />} />
+        <Route path="/become-good-voter" element={<VoterGuide />} />
+        <Route path="/become-good-party" element={<PartyArticle />} />
+        <Route path="/voter-homepage" element={<VoterHomepage />} />
+        <Route path="/cast-vote" element={<CastVote />} />
+        <Route path="/know-your-candidate" element={<KnowYourCandidate />} />
       </Routes>
     </div>
   );
